3058aefb41ddf6aa2e6b627fa84a986f3f872279
adam.hall <adam.hall@perkbox.com>
2023-04-18 20:09:06 +0100

    fix: no longer require missing parsers

--- lua/qmk/parse/zmk/queries.lua
+++ lua/qmk/parse/zmk/queries.lua
@@ -3,25 +3,6 @@ local parse_query = require('qmk.parse.visitor').parse_query
 
 local M = {}
 
----look through the whole file and find all layouts
----including their names
----zmk is a little more dynamic than qmk, so we just grab
----everything in the bindings node and iterate through the
----children
----@type Query
-local keymap_query = parse_query(
-	'devicetree',
-	[[
-(node
-    name: (identifier) @_nodeid (#eq? @_nodeid keymap)
-    (node
-        name: (identifier) @layer_name
-        (property
-            name: (identifier) @_propid (#eq? @_propid bindings)
-            value: (_ (">" @final)) @bindings)))
-]]
-)
-
 M.keymap_ids = {
 	layer_name = 'layer_name',
 	bindings = 'bindings',
@@ -32,6 +13,27 @@ M.keymap_ids = {
 ---@param content string
 ---@param visitors table<string, fun(node: tsnode): nil>
 function M.keymap_visitor(root, content, visitors)
+	--TODO: pull out and memo later
+
+	---look through the whole file and find all layouts
+	---including their names
+	---zmk is a little more dynamic than qmk, so we just grab
+	---everything in the bindings node and iterate through the
+	---children
+	---@type Query
+	local keymap_query = parse_query(
+		'devicetree',
+		[[
+(node
+    name: (identifier) @_nodeid (#eq? @_nodeid keymap)
+    (node
+        name: (identifier) @layer_name
+        (property
+            name: (identifier) @_propid (#eq? @_propid bindings)
+            value: (_ (">" @final)) @bindings)))
+]]
+	)
+
 	visitor(keymap_query, root, visitors, content)
 end
 
