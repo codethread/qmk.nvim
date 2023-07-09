e402c1d822cff21a9f3b0ebf8b8816f6cf393d36
codethread <adamhalldesigns@gmail.com>
2023-07-09 22:42:24 +0100

    test

---
+++ test/fixtures/qmk/numbers.c
@@ -0,0 +1,11 @@
+// clang-format off
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+[0] = LAYOUT_preonic_grid(
+      KC_1 , 
+    KC_2 , KC_3 ,
+    KC_4 ,   KC_5
+),
+[_FOO] = LAYOUT_preonic_grid(
+      KC_1 ,    KC_2 , KC_3 , KC_4 , KC_5
+)
+};
