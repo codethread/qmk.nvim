67c1a94b10f7266ac01b0a2431dade70693edba9
Adam Hall <adamhalldesigns@gmail.com>
2024-02-15 21:12:27 +0000

    fix(zmk): support position.bottom (#37)
    * fix(zmk): support position.bottom
    
    * Auto generate docs
    
    * doc update
    
    * Auto generate docs
    
    ---------
    
    Co-authored-by: codethread <codethread@users.noreply.github.com>

--- lua/qmk/commands/format.lua
+++ lua/qmk/commands/format.lua
@@ -1,12 +1,19 @@
 local parser = require('qmk.parse')
 local api = vim.api
 
+---@param options qmk.Config
+---@param content string[]
+---@param bufnr number
 local function qmk(options, content, bufnr)
 	local keymaps, config = parser.parse(table.concat(content, '\n'), options, parser.qmk)
 	local formatted = require('qmk.format.qmk')(keymaps, config)
 	api.nvim_buf_set_lines(bufnr, keymaps.pos.start + 1, keymaps.pos.final, false, formatted)
 end
 
+---@param options qmk.Config
+---@param content string[]
+---@param bufnr number
+---@param last_keymap? number
 local function zmk(options, content, bufnr, last_keymap)
 	local keymap_id = last_keymap or 1
 	local keymaps = parser.parse(table.concat(content, '\n'), options, parser.zmk).keymaps
@@ -14,8 +21,13 @@ local function zmk(options, content, bufnr, last_keymap)
 	local keymap = keymaps[keymap_id]
 	if keymap ~= nil then
 		local out = require('qmk.format.zmk')(keymap, options)
-		vim.list_extend(out.preview, out.keys)
-		api.nvim_buf_set_lines(bufnr, out.pos.start + 1, out.pos.final, false, out.preview)
+		if options.comment_preview.position == 'bottom' then
+			vim.list_extend(out.keys, out.preview)
+			api.nvim_buf_set_lines(bufnr, out.pos.start + 1, out.pos.final, false, out.keys)
+		else
+			vim.list_extend(out.preview, out.keys)
+			api.nvim_buf_set_lines(bufnr, out.pos.start + 1, out.pos.final, false, out.preview)
+		end
 
 		-- we just reparse over and over till all keymaps are done
 		local new_content = api.nvim_buf_get_lines(bufnr, 0, -1, false)
