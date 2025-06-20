1715bc964daea67bad0f32f55de1c575482556fd
Oula Kuuva <oula.kuuva@iki.fi>
2025-06-20 15:44:07 +0300

    feat(overrides): auto-escape magic characters in keys
    No more adding % before every () in the override definitions \o/ Old
    patterns still work too, thanks to logic that does not escape already
    escaped characters.

--- lua/qmk/format/get_key_text.lua
+++ lua/qmk/format/get_key_text.lua
@@ -1,14 +1,17 @@
+local utils = require('qmk.format.utils')
+
 ---find all matching key codes from the key string and replace them with the keymap value
 ---@param keymap qmk.KeymapList
 ---@return fun (key : string): string
 local function get_key_text(keymap)
-	return function(key)
-		local str = key
+	return function(str)
 		for _, k in ipairs(keymap) do
+      -- escape lua magic characters in the matching pattern
+      local key, value = utils.escape_magic_characters(k.key), k.value
 			-- check if the key is a substring of the current key
-			if string.find(str, k.key) then
+			if string.find(str, key) then
 				-- replace the key with the override
-				str = string.gsub(str, k.key, k.value)
+				str = string.gsub(str, key, value)
 			end
 		end
 		return str
